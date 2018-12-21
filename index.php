9502f1ea3ea9272f25e1c2734f7569a73845fa76
ahmedsamka <35009636+ahmedsamka@users.noreply.github.com>
2018-12-22 00:25:46 +0200

    App

---
+++ index.php
@@ -0,0 +1,364 @@
+<?php include 'Public/header.php'; ?>
+	<!-- ========= Main Left list========== -->
+	<div class="MuneLeft">
+		<ul>
+			<li>
+				<a href="users.php">	
+					<span class="text-mune">
+						<span><i class="fas fa-at"></i> </span> 
+					My Profile</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="fas fa-home"></i></span> 
+					Feed</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="far fa-bell"></i></span> 
+					Notifications</span>
+					<span class="m_n_new">99+</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">	
+						<span><i class="far fa-comment-alt"></i></span>
+					Messages</span>
+					<span class="m_n_new">99+</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="fas fa-camera-retro"></i></span> 
+					Photos</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="fas fa-video"></i></span>
+					Videos</span>
+				</a>
+																	
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="fas fa-music"></i></span> 
+					Audios</span>
+				</a>
+			</li>
+			<div class="more_div l_main"></div>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="far fa-bookmark"></i></span> Bookmarks
+					</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="far fa-file"></i></span> 
+						<!-- <img src="Public/assets/image/pincel.png"> -->
+						Note
+					</span>
+				</a>
+			</li>
+			<!-- <li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="fas fa-gamepad"></i></span> Games
+					</span>
+				</a>
+			</li> -->
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="far fa-edit"></i></span> Edit profile
+					</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<span class="text-mune">
+						<span><i class="fas fa-cogs"></i></span> Settings
+					</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<!-- /==========End Main Left list ==========/ -->
+	<!-- /====================/ -->
+	<div class="bodyPage">
+		<div class="bodyCenter">
+			<div class="MuneCenter">
+				<!-- ================ createPostAndSher ================== -->
+				<div class="timeline-post">       
+			        <div class="post-type">
+			          <img class="top-line " src="https://pbs.twimg.com/profile_images/922004311818821633/aw8JfgVz_normal.jpg">
+			          
+			          <form class="post-form" method="post" onsubmit="return false;">       
+			          	<textarea class="create-post" oninput="eylem(this,this.value)" placeholder="Write something.....#hashtags @mentions"></textarea>
+
+			            <!-- /////// -->
+			            <div class="top-icon">
+			              <div class="Emoji">
+			                <i class="far fa-smile"></i>
+			              </div>
+			            </div>
+			            <div class="top-icon">
+			              <div class="Emoji">
+			                <i class="far fa-image"></i>
+			              </div>
+			            </div>
+			           <!-- /////// -->
+			           <!-- <input type="submit" class="share-post" name="post" value="Status" id="btn-sher"> -->
+			           <button type="submit" class="share-post"> Status <i class="fas fa-arrow-right"></i></button>
+			           
+			            <div id="countPost">180</div>
+			           
+			          </form>
+
+			        </div>
+
+		        </div>
+		        <!-- ================ Emd createPostAndSher ================== -->
+		        	<!-- ================================================= -->
+		        <div class="itemPost">
+		        	<!-- ========================= -->
+		        	<div class="conPost">
+		        		<a href="#">
+		        			<img src="https://pbs.twimg.com/profile_images/922004311818821633/aw8JfgVz_bigger.jpg">
+		        		</a>
+		        	</div>
+		        	<!-- ========================= -->
+		        	<div class="contName">
+		        		<a href="#">
+		        			<h3>Ahmed Adb Elkailq</h3>
+		        			<span>@samka</span>
+		        		</a>
+		        		<span>&nbsp; . 3h</span>
+		        	</div>
+		        	<!-- ========================= -->
+		        	<span class="togglePost"><i class="fas fa-chevron-down"></i></span>
+		        	<!-- ========================= -->
+		        	<div class="textPost">
+		        		<p>الولد: شغلي مش حرام
+المذيعة: لا حرام انت كده بتضر بلدك
+الولد: يجي ايه جنب اللي بيتهرب 
+انتوا بتمسكوا العيال وتسيبوا المهربين الكبار
+اكسي  امي و اخواتي و اكلهم منين
+مفيش شغل فالبلد انتي مش حاسه بالناس</p>
+		        	</div>
+		        	<!-- ========================= -->
+		        	<div class="footerItem">
+
+		        		<div class="stut-action-lk-s">
+		        			<span class="likPost">
+			        			<i class="material-icons">arrow_upward</i> Like
+			       				
+			        		</span>
+			        		<span id="rowCont-post">1500</span>
+		        		</div>
+		        		
+		        		<!-- <div class="stut-action"> -->
+			        		<span class="likPostRep">
+			        			<i class="material-icons">compare_arrows</i> Republish
+			        			
+			        		</span>
+		        			<span id="rowCont-postRep">1500</span>
+		        		<!-- </div> -->
+
+		        		<span class="comntPost">
+		        			<!-- <i class="far fa-comment"></i> -->
+		        			<!-- <i class="fas fa-arrow-circle-right"></i> -->
+		        			<i class="fas fa-chevron-circle-right"></i> Reply
+		        		</span>
+		        		<span id="rowCont-postCom">1500</span>
+		        	</div>
+		        	<!-- ========================= -->
+		        </div>
+		        <!-- ========================================= -->
+		        			        <div class="itemPost">
+		        	<!-- ========================= -->
+		        	<div class="conPost">
+		        		<a href="#">
+		        			<img src="https://pbs.twimg.com/profile_images/922004311818821633/aw8JfgVz_bigger.jpg">
+		        		</a>
+		        	</div>
+		        	<!-- ========================= -->
+		        	<div class="contName">
+		        		<a href="#">
+		        			<h3>Ahmed Adb Elkailq</h3>
+		        			<span>@samka</span>
+		        		</a>
+		        		<span>&nbsp; . 3h</span>
+		        	</div>
+		        	<!-- ========================= -->
+		        	<span class="togglePost"><i class="fas fa-chevron-down"></i></span>
+		        	<!-- ========================= -->
+		        	<div class="textPost">
+		        		<p>الولد: شغلي مش حرام
+المذيعة: لا حرام انت كده بتضر بلدك
+الولد: يجي ايه جنب اللي بيتهرب 
+انتوا بتمسكوا العيال وتسيبوا المهربين الكبار
+اكسي  امي و اخواتي و اكلهم منين
+مفيش شغل فالبلد انتي مش حاسه بالناس</p>
+		        	</div>
+		        	<!-- ========================= -->
+		        	<div class="footerItem">
+		        		<span class="likPost">
+		        			<i class="material-icons">arrow_upward</i> Like
+		       
+		        		</span>
+		        		<span id="rowCont-post">1500</span>
+		        		
+		        		<span class="likPostRep">
+		        			<i class="material-icons">compare_arrows</i> Republish
+		        			
+		        		</span>
+		        		<span id="rowCont-postRep">1500</span>
+		        		<span class="comntPost">
+		        			<i class="far fa-comment"></i> Reply
+		        		</span>
+		        		<span id="rowCont-postCom">1500</span>
+		        	</div>
+		        	<!-- ========================= -->
+		        </div>
+			</div>
+			<!-- /====================/ -->
+			<!-- /========Profile Panle Link ============/ -->
+			<div class="MuneRight">
+				<!-- ====================== -->
+				<div class="profileLink">
+					<div class="dash-link">
+						<a href="#">
+							<img src="https://pbs.twimg.com/profile_images/922004311818821633/aw8JfgVz_bigger.jpg">
+						</a>
+					</div>
+					<!-- ================= -->
+					<div class="dash-name">
+						<div class="dash-name-spn"><a href="#">Ahmed Adb Elkailq</a></div>
+						<span><a href="#">@samka</a></span>
+					</div>
+					<!-- ================= -->
+					<div class="tabl-row	">
+						<ul>
+							<li>
+								<a href="#">
+									<span class="prf-crd">POSTS</span>
+									<span class="prf-crd-tb">100</span>
+								</a>
+							</li>
+							<li>
+							    <a href="#">
+							        <span class="prf-crd">LIKS</span>
+							        <span class="prf-crd-tb">500</span>
+							    </a>
+						    </li>
+						    <li>
+						        <a href="#">
+							        <span class="prf-crd">PHOTO</span>
+							        <span class="prf-crd-tb">100</span>
+						        </a>
+						    </li>
+						</ul>
+					</div>
+					<!-- ========================= -->
+				</div>
+				<!-- ====================== -->
+				<!-- ===========Suggested List =========== -->
+				<div class="suggDiv">
+					<div class="suggHead">
+						<span>Suggested people</span>
+					</div>
+					<div class="suggBody">
+						<!-- ============================ -->
+						<div class="userCound">
+							<div class="userRow">
+								<a href="#">
+									<img src="https://pbs.twimg.com/profile_images/922004311818821633/aw8JfgVz_bigger.jpg">
+									<span class="user-block">
+										<strong>Ahmed Adb Elkailq</strong>
+										<span>@samka</span>
+									</span>
+								</a>
+								<button class="btn-follow" type="button">
+									<!-- <span>Follow</span> -->
+									Follow
+								</button>
+							</div>
+						</div>
+						<!-- ============================ -->
+						<div class="userCound">
+							<div class="userRow">
+								<a href="#">
+									<img src="https://pbs.twimg.com/profile_images/922004311818821633/aw8JfgVz_bigger.jpg">
+									<span class="user-block">
+										<strong>Ahmed Adb Elkailq</strong>
+										<span>@samka</span>
+									</span>
+								</a>
+								<button class="btn-follow" type="button">
+									<!-- <span>Follow</span> -->
+									Follow
+								</button>
+							</div>
+						</div>
+						<!-- ============================ -->
+						<!-- ============================ -->
+						<div class="userCound">
+							<div class="userRow">
+								<a href="#">
+									<img src="https://pbs.twimg.com/profile_images/922004311818821633/aw8JfgVz_bigger.jpg">
+									<span class="user-block">
+										<strong>Ahmed Adb Elkailq</strong>
+										<span>@samka</span>
+									</span>
+								</a>
+								<button class="btn-follow" type="button">
+									<!-- <span>Follow</span> -->
+									Follow
+								</button>
+							</div>
+						</div>
+						<!-- ============================ -->
+					</div>
+				</div>
+				<!-- ====================== -->
+				<!-- ============Hashtag List ============= -->
+				<div class="suggDiv">
+					<div class="suggHeadHash">
+						<span>Popular trends</span>
+					</div>
+					<div class="hasBlock">
+						<div class="my-trend">
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>	
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+							<a href="#"><i class="fab fa-slack"></i> Hashtag_Hashtag</a>
+						</div>
+						
+					</div>
+				</div>
+				<!-- =========================== -->
+			</div>
+		</div>
+	</div>
+	<!-- ==========body page center ============= -->
+	</div>
+</body>
+</html>
---
+++ users.php
@@ -0,0 +1,20 @@
+<?php include 'Public/header.php'; ?>
+<div class="ui-block">
+	<div class="top-header">
+		<div class="top-header-baner">
+			<img src="https://html.crumina.net/html-olympus/img/top-header1.jpg">
+		</div>
+		<div class="top-header-bar">
+			test
+		</div>
+		<div class="top-header-author">
+			<a href="02-ProfilePage.html" class="author-thumb">
+				<img src="https://html.crumina.net/html-olympus/img/author-main1.jpg" alt="author">
+				<div class="author-content">
+					<a href="#">James Spiegel</a>
+					<div class="country">San Francisco, CA</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</div>
